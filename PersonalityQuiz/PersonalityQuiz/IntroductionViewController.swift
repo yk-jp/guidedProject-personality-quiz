8dcb68d549c62dfeb693891f6a213f1472fbd0cb
yk-jp <k82.y12@gmail.com>
2022-05-06 16:32:14 -0700

    finish part3

---
+++ PersonalityQuiz/PersonalityQuiz/IntroductionViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Yusuke K on 2022-05-05.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
